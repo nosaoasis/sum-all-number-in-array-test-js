3dc1a35ef540d035450c8bee41c0c940584b3ac8
Nosa Agho <nosaoasis@gmail.com>
2023-04-26 20:08:56 +0100

    Sum Nested Integer Array Using Recursion
    Implemented the function 'sumItems' which takes an array as parameters and returns the sum of the numbers in the array. The function is implemented using recursion.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  
+  /* // non-nested array
+  if (array.length === 0) return 0
+  return array.shift() + sumItems(array)
+  */ 
+
+  // nested array
+  let sum = 0;
+  array.forEach((item) => {
+    if(Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+    sum += item;
+    }
+  })
+  return sum;
+
 }
+
+console.log("sum of array items value is", sumItems([1, 2, 3, 4, 5]))
+console.log("sum of array items value is", sumItems([[1, 2, [[3], 4]], 5, []]))
+console.log("sum of array items value is", sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]))
 
 module.exports = sumItems;
